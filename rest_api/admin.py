25b90a8fab2289eb24ce8c52e3ca404ed1ab9bc4
Chuks-onah Benedict <48535419+itachi1010@users.noreply.github.com>
2023-01-29 13:42:03 +0000

    Initial commit

---
+++ rest_api/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Language
+
+
+# Register your models here.
+
+class LanguageAdmin(admin.ModelAdmin):
+    list = ('name', 'paradigm', 'author')
+
+    admin.site.register(Language)
